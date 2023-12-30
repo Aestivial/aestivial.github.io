a9c8da2b75dbc22ac0fc565c42c1afb65b05b169
Aestivial <nayanpanda5963@gmail.com>
2023-12-30 19:08:03 +0530

    Update

--- main/script.js
+++ main/script.js
@@ -6,17 +6,26 @@ if(!localStorage.getItem('loggedInUser')){
   function handleEnter(event) {
     if (event.keyCode === 13) {
       const command = document.getElementById("command-input").value;
+  
       if (pages.hasOwnProperty(command)) {
-        window.location.href = pages[command];
+        // Check if the command is "portfolio"
+        if (command === "portfolio") {
+          // Open the external website in a new tab
+          window.open("https://aestivial.github.io/portfolio/", "_blank");
+        } else {
+          // Open the corresponding page in a new tab
+          window.open(pages[command], "_blank");
+        }
       } else {
+        // Display error message for unknown commands
         document.getElementById("terminal-output").innerHTML = `<p>Error: Command not found: ${command}</p>`;
       }
     }
   }
   
-  
   const pages = {
     'home': '../sub/home/home.html',
+    'portfolio': '../sub/portfolio/index.html',
     'about': '../Old Site/Resume/resume.html',
     'projects': '../sub/projects.html',
     'contact': '../sub/contact/contact.html',
